d6d47f0557558f8ed1621abef3b60bfc4cae11a9
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-08-04 14:55:35 +0900

    add: ignore comments for explicit any.

--- app/env.d.ts
+++ app/env.d.ts
@@ -2,12 +2,14 @@
 /// <reference types="vite/client" />
 
 declare module "*.mdx" {
+  // biome-ignore lint:suspicious/noEplicitAny
   let MDXComponent: (props: any) => JSX.Element;
+  // biome-ignore lint:suspicious/noEplicitAny
   export const frontmatter: any;
   export default MDXComponent;
 }
 
 declare module "virtual:remix/server-build" {
-  import { ServerBuild } from "@remix-run/node";
+  import type { ServerBuild } from "@remix-run/node";
   export const routes: ServerBuild["routes"];
 }
