6c944f7cfe24f975757d8160b069f2f59c2f5e63
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-08-10 09:45:13 +0900

    add: support remix.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
 import mdx from "@mdx-js/rollup";
-import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from "@remix-run/dev";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default defineConfig({
+  base: "/my-site/",
   plugins: [
     mdx({ remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter] }),
-    remixCloudflareDevProxy(),
     remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
       // https://remix.run/docs/en/main/guides/spa-mode
       ssr: false,
+
+      // https://zenn.dev/cybozu_frontend/articles/remix-spa-mode-gh-page
+      basename: "/my-site/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
+      },
     }),
     tsconfigPaths(),
   ],
