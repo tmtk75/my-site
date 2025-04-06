68b7e672c72c72732377dfcd198a0d8513725344
Tomotaka Sakuma <ktmtmks@gmail.com>
2025-04-06 22:15:27 +0900

    fix: buildDirectory.

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
 	basename: "/",
-	buildDirectory: "../../out/renderer",
+	// buildDirectory: "../../out/renderer",
 	ssr: false,
 } satisfies Config;
